01fd3ddfa7ad32fccb82473e0bf0e43f4eb4baef
Matthew Wilson <mtwilson2022@gmail.com>
2025-02-27 11:47:55 -0700

    Finished user service tests

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,42 +1,100 @@
 package service;
 
-import dataaccess.MemUserDAO;
-import dataaccess.UserDAO;
-import dataaccess.MemAuthDAO;
-import dataaccess.AuthDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dataaccess.*;
 
 import model.AuthData;
 import model.UserData;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import response.*;
+import service.UserService;
+import request.*;
 
 import java.util.UUID;
 
 public class UserServiceTests {
 
-    // instance vars
-
+    UserDAO userDAO;
+    AuthDAO authDAO;
+    UserData registeredUser;
+    UserService service;
 
     @BeforeEach
     public void setUp() {
-        UserDAO userDAO = new MemUserDAO(); // change to SQLUserDAO in phase 4?
-        AuthDAO authDAO = new MemAuthDAO(); // change to SQLAuthDAO in phase 4?
-        var service = new UserService(userDAO, authDAO);
+        this.userDAO = new MemUserDAO(); // change to SQLUserDAO in phase 4?
+        this.authDAO = new MemAuthDAO(); // change to SQLAuthDAO in phase 4?
+        this.service = new UserService(userDAO, authDAO);
 
         // add something to userDAO and authDAO
-        UserData registeredUser = new UserData("user1", "password", "email@example.com");
+        this.registeredUser = new UserData("user1", "password", "email@example.com");
         userDAO.insertUser(registeredUser);
-
-        String authToken = UUID.randomUUID().toString();
-
-        AuthData userAuth = new AuthData(authToken, "user1");
-        authDAO.insertAuth(userAuth);
     }
 
     @Test
     public void successLogin() {
+        var req = new LoginRequest("user1", "password");
+        try {
+            Assertions.assertNotNull(service.login(req));
+        } catch (UnauthorizedException e) {
+            Assertions.fail("Login should not have thrown an exception.");
+        }
+    }
 
+    @Test
+    public void failLogin() {
+        var requestWrongPassword = new LoginRequest("user1", "not right");
+        Assertions.assertThrows(UnauthorizedException.class, () -> {
+            service.login(requestWrongPassword);
+        });
+
+        var requestNotAUser = new LoginRequest("not a registered user", "abc");
+        Assertions.assertThrows(UnauthorizedException.class, () -> {
+            service.login(requestNotAUser);
+        });
+    }
+
+    @Test
+    public void successRegister() {
+        try {
+            var req = new RegisterRequest("new user", "12345678", "new_email@site");
+            var resp = service.register(req);
+            Assertions.assertEquals("new user", resp.username());
+            Assertions.assertNotNull(authDAO.getAuth(resp.authToken()));
+        } catch (BadRequestException bre) {
+            throw new RuntimeException("Invalid input");
+        } catch (AlreadyTakenException ate) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Test
+    public void failRegister() {
+        var requestAlreadyAUser = new RegisterRequest("user1", "new password", "rand_email");
+        Assertions.assertThrows(AlreadyTakenException.class, () -> {
+            service.register(requestAlreadyAUser);
+        });
+    }
+
+    @Test
+    public void successLogout() {
+        String token = "some_token"; // need to figure out how to get the right token
+        var testAuthData = new AuthData(token, "user1");
+        authDAO.insertAuth(testAuthData);
+
+        var req = new LogoutRequest(token);
+        Assertions.assertDoesNotThrow(() -> service.logout(req));
+    }
+
+    @Test
+    public void failLogout() {
+        String token = "bad token";
+        var req = new LogoutRequest(token);
+        Assertions.assertThrows(UnauthorizedException.class, () -> {
+            service.logout(req);
+        });
     }
 }
