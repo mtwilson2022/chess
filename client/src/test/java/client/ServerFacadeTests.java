660ad754b7c299c2f22df978c251723811de541b
mtwilson2022 <mtwilson2022@gmail.com>
2025-03-22 15:31:39 -0600

    Started ServerFacade tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,17 +2,23 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        String url = "https://localhost:" + port;
+        facade = new ServerFacade(url);
     }
 
     @AfterAll
@@ -22,8 +28,69 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void successRegister() {
+        var registerResult = facade.register("player1", "password", "p1@email.com");
+        assertTrue(registerResult.authToken().length() > 10);
+        assertEquals("player1", registerResult.username());
     }
 
+    @Test
+    void failRegister() {
+
+    }
+
+    @Test
+    void successLogin() {
+
+    }
+
+    @Test
+    void failLogin() {
+
+    }
+
+    @Test
+    void successLogout() {
+
+    }
+
+    @Test
+    void failLogout() {
+
+    }
+
+    @Test
+    void successListGames() {
+
+    }
+
+    @Test
+    void failListGames() {
+
+    }
+
+    @Test
+    void successCreateGame() {
+
+    }
+
+    @Test
+    void failCreateGame() {
+
+    }
+
+    @Test
+    void successJoinGame() {
+
+    }
+
+    @Test
+    void failJoinGame() {
+
+    }
+
+    @Test
+    void successClear() {
+
+    }
 }
